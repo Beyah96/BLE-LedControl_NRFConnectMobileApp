73bf4ff7cafc91f4633506c4ab888035e125a956
Beyah96 <bahsaid96@gmail.com>
2024-12-08 00:00:52 +0100

    Updating files

--- src/main.c
+++ src/main.c
@@ -1,43 +1,71 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gatt.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
-
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-int main(void)
+static uint8_t led_state = 0;
+
+static ssize_t write_led_state(struct bt_conn *conn,
+                               const struct bt_gatt_attr *attr,
+                               const void *buf, uint16_t len,
+                               uint16_t offset, uint8_t flags)
 {
-	int ret;
+    if (len != 1) {
+        return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
+    }
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+    led_state = *((uint8_t *)buf);
+    gpio_pin_set_dt(&led, led_state);
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
+    return len;
+}
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
-		k_msleep(SLEEP_TIME_MS);
-	}
-	return 0;
+BT_GATT_SERVICE_DEFINE(led_svc,
+    BT_GATT_PRIMARY_SERVICE(BT_UUID_DECLARE_16(0x1234)),
+    BT_GATT_CHARACTERISTIC(BT_UUID_DECLARE_16(0x5678),
+                           BT_GATT_CHRC_WRITE,
+                           BT_GATT_PERM_WRITE,
+                           NULL, write_led_state, NULL),
+);
+
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0x34, 0x12),
+};
+
+void main(void)
+{
+    int err;
+
+    if (!device_is_ready(led.port)) {
+        printk("LED device not ready\n");
+        return;
+    }
+
+    err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (err < 0) {
+        printk("Error configuring LED pin: %d\n", err);
+        return;
+    }
+
+    err = bt_enable(NULL);
+    if (err) {
+        printk("Bluetooth init failed (err %d)\n", err);
+        return;
+    }
+
+    err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+    if (err) {
+        printk("Advertising failed to start (err %d)\n", err);
+        return;
+    }
+
+    printk("Bluetooth initialized and advertising\n");
+
+    while (1) {
+        k_sleep(K_FOREVER);
+    }
 }
